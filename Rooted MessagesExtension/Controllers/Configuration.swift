33402108a061ba0f69e5efd8b97c79cec22b584e
Michael Westbrooks <mwestbrooks@salesforce.com>
2020-02-26 00:35:18 -0500

    Updated folder structure and hierarchy

---
+++ Rooted MessagesExtension/Controllers/Configuration.swift
@@ -0,0 +1,9 @@
+//
+//  Configuration.swift
+//  Rooted MessagesExtension
+//
+//  Created by Michael Westbrooks on 2/26/20.
+//  Copyright © 2020 RedRooster Technologies Inc. All rights reserved.
+//
+
+import Foundation
