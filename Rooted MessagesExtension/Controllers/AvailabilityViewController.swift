e8080e222994fa4be33f4f64d62f86ccf878e1a0
Michael Westbrooks <mwestbrooks@salesforce.com>
2020-03-08 14:51:21 -0400

    Added "AddAvailabilityViewController" to prepare for creating availability

---
+++ Rooted MessagesExtension/Controllers/AvailabilityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AvailabilityViewController.swift
+//  Rooted MessagesExtension
+//
+//  Created by Michael Westbrooks on 3/8/20.
+//  Copyright © 2020 RedRooster Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class AvailabilityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
